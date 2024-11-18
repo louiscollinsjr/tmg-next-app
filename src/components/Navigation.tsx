5643c4e9642670e11d990ab38198adb546d986fe
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-18 12:22:58 +0200

    updated nav colors

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,7 +8,7 @@ export default function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="sticky top-0 z-50 bg-white">
+    <nav className="sticky top-0 z-50 bg-primary">
       <div className="max-w-[61.25rem] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-[44px]">
           {/* Logo */}
