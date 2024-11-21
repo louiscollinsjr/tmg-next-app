8a66f4daebac7c68d18621e2756ad4ab30863bc4
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-21 23:47:21 +0200

    update...

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -32,13 +32,25 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any'
+      },
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
       },
       {
         src: '/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
       },
     ],
     shortcuts: [
