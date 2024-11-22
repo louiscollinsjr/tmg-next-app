9fb3b74a7677f8b421b915e357e98a86fe45e577
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-22 09:44:00 +0200

    manifest files fix

--- next.config.js
+++ next.config.js
@@ -11,6 +11,23 @@ const nextConfig = {
       'example.com'
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true'
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
